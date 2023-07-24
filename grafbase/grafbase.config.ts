e68c41e45d35fcd9a86066d41f192489ce1eef68
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-07-24 08:27:13 -0400

    updated actions, session, graphql to create users and sign in users

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk';
 
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
@@ -11,8 +12,12 @@ const User = g.model('User', {
     .relation(() => Project)
     .list()
     .optional(),
+})
+.auth((rules) => {
+  rules.public().read();
 });
 
+// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(),
@@ -21,13 +26,21 @@ const Project = g.model('Project', {
   githubUrl: g.url(),
   category: g.string(),
   createdBy: g.relation(() => User),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
 });
 
 const jwt = auth.JWT({
   issuer: 'grafbase',
-  secret: g.env('NEXTAUTH_SECRET'),
+  // secret: g.env('NEXTAUTH_SECRET'),
+  secret: '{{ env.NEXTAUTH_SECRET }}',
 });
 
 export default config({
   schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private(),
+  },
 });
--- lib/actions.ts
+++ lib/actions.ts
@@ -4,7 +4,7 @@ import { GraphQLClient } from 'graphql-request';
 const isProduction = process.env.NODE_ENV === 'production';
 const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
-const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL || '' : 'http://lcoalhost:3000';
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL || '' : 'http://localhost:3000';
 
 const client = new GraphQLClient(apiUrl);
 
@@ -17,10 +17,12 @@ const makeGraphQLRequest = async (query: string, variables = {}) => {
 };
 
 export const getUser = (email: string) => {
+  client.setHeader('x-api-key', apiKey);
   return makeGraphQLRequest(getUserQuery, { email });
 };
 
 export const createUser = (name: string, email: string, avatarUrl: string) => {
+  client.setHeader('x-api-key', apiKey);
   const variables = {
     input: {
       name: name,
