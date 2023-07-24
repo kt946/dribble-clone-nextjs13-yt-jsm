4bc40885ff19789e707275fd05a0bed52d6d811b
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-07-24 07:01:19 -0400

    added graphql and actions and updated session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -4,6 +4,8 @@ import { AdapterUser } from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 import jsonwebtoken from 'jsonwebtoken';
 import { JWT } from 'next-auth/jwt';
+import { SessionInterface, UserProfile } from '@/common.types';
+import { createUser, getUser } from './actions';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -22,12 +24,33 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async session({ session }) {
-      return session;
+      const email = session?.user?.email as string;
+
+      try {
+        const data = (await getUser(email)) as { user?: UserProfile };
+
+        // If user exists, merge user data into the existing session
+        const newSession = {
+          ...session,
+          user: {
+            ...session.user,
+            ...data?.user,
+          },
+        };
+
+        return newSession;
+      } catch (error: any) {
+        console.error('Error retrieving user data: ', error.message);
+        return session;
+      }
     },
     async signIn({ user }: { user: AdapterUser | User }) {
       try {
-        // get the user if they exist
-        // if they don't exist, create them
+        const userExists = (await getUser(user?.email as string)) as { user?: UserProfile };
+
+        if (!userExists.user) {
+          await createUser(user.name as string, user.email as string, user.image as string);
+        }
         return true;
       } catch (error) {
         console.log(error);
@@ -36,3 +59,9 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+
+export async function getCurrentUser() {
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+
+  return session;
+}
