90562934edcbd1ac321aced0f8e65af634cbe6a4
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-07-26 21:59:51 -0400

    updated ProjectForm with FormField and CustomMenu components

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,9 +1,100 @@
-import React from 'react'
+'use client';
 
-const ProjectForm = () => {
+import { SessionInterface } from '@/common.types';
+import Image from 'next/image';
+import { ChangeEvent, useState } from 'react';
+import FormField from './FormField';
+import { categoryFilters } from '@/constants';
+import CustomMenu from './CustomMenu';
+
+type Props = {
+  type: string;
+  session: SessionInterface;
+};
+
+const ProjectForm = ({ type, session }: Props) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+  const handleStateChange = (fieldName: string, value: string) => {};
+
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    image: '',
+    liveSiteUrl: '',
+    githubUrl: '',
+    category: '',
+  });
+
   return (
-    <div>ProjectForm</div>
-  )
-}
+    <form
+      onSubmit={handleFormSubmit}
+      className="flexStart form"
+    >
+      <div className="flexStart form_image-container">
+        <label
+          htmlFor="poster"
+          className="flexCenter form_image-label"
+        >
+          {!form.image && 'Choose a poster for your project'}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === 'create'}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-20"
+            alt="Project poster"
+            fill
+          />
+        )}
+      </div>
 
-export default ProjectForm
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange('title', value)}
+      />
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects"
+        setState={(value) => handleStateChange('description', value)}
+      />
+      <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://vercel.com/"
+        setState={(value) => handleStateChange('liveSiteUrl', value)}
+      />
+      <FormField
+        type="url"
+        title="GitHub URL"
+        state={form.githubUrl}
+        placeholder="https://github.com/kt946"
+        setState={(value) => handleStateChange('githubUrl', value)}
+      />
+
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange('category', value)}
+      />
+
+      <div className="flexStart w-full">
+        <button>Create</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProjectForm;
